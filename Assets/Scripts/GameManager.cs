fbfdd18fd1d8d8befd6bb206fee4a60b197d0354
Bogdan <b.konnov00@gmail.com>
2024-08-18 01:37:46 +0300

    Small general fixes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : BaseManager
 {
+    private List<LevelData> _availableLevels;
+    private List<BonusData> _availableBonuses;
+    private List<PlayerData> _availableSkins;
     public override IEnumerator Init()
     {
         return base.Init();
